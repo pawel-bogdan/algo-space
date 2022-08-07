8a4fade08da9c90d7af650277c0cb27b6e93f5c4
OmanDominik <dominoma013@gmail.com>
2022-08-07 23:53:33 +0200

    initial commit

---
+++ src/main/java/zpi/algospace/AlgoSpaceApplication.java
@@ -0,0 +1,13 @@
+package zpi.algospace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zpi/algospace/AlgoSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package zpi.algospace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
