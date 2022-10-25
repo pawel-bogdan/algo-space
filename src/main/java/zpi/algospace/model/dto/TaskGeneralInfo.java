b7ca34e05edc9cb100e6750913cc6a65b0f03ede
Dominik Omański <dominoma013@gmail.com>
2022-10-25 19:29:39 +0200

    template endpoint

--- src/main/java/zpi/algospace/model/dto/TaskGeneralInfo.java
+++ src/main/java/zpi/algospace/model/dto/TaskGeneralInfo.java
@@ -2,13 +2,12 @@ package zpi.algospace.model.dto;
 
 import lombok.Builder;
 import lombok.Getter;
-import zpi.algospace.model.Category;
 
 @Builder
 @Getter
 public class TaskGeneralInfo {
     private Long id;
     private String name;
-    private Category category;
+    private CategoryDTO category;
     private DifficultyDTO difficulty;
 }
